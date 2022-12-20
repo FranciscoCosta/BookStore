6f1a518854dd8598938ad908df5a610f7c7dac35
Xico <francisco100eg@gmail.com>
2022-12-20 12:31:47 -0300

    Feat: Added routes

--- Client/bookstore/src/App.js
+++ Client/bookstore/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import { Books } from './pages/Books';
+import { Add } from './pages/Add';
+import { Update } from './pages/Update';
+
 
 function App() {
   return (
     <div className="App">
-      Hello
-
+      <Routes>
+        <Route path='/' element={<Books/>} />
+        <Route path='/' element={<Add/>} />
+        <Route path='/' element={<Update/>} />
+      </Routes>
     </div>
   );
 }
